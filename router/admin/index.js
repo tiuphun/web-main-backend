b737528ece54962949fdc09c84dc30885140cc2b
duc0905 <duc20022009@gmail.com>
2020-02-02 09:41:50 +0700

    add passport

--- router/admin/index.js
+++ router/admin/index.js
@@ -1,26 +1,44 @@
 const Router = require('express').Router();
 const request = require('request');
 const helpers = require('../../helpers');
-const passport = require('passport');
-const {User} = require('../../mongoose_models');
 
 // Router.use(authenticate);
 
 Router.post("/login", (req, res) => {
-    console.log(req);
+    console.log(JSON.stringify(req.body));
+    let options = {
+        url: 'http://localhost:4000/login',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req.body)
+    }
+    request.post(options, (err, response) => {
+        if(err) {
+            return res.sendStatus(403);
+        }
+        if(response.statusCode >= 400) {
+            return res.json({
+                err: "Forbidden",
+                message: "Incorrect username or password"
+            });
+        }
+        res.json(JSON.parse(response.body))
+    })
+
 })
 
 Router.post("/posts", (req, res) => {
     console.log(req.cookies);
-    // helpers.createPost({...req.body, author: req.user.name})
-    // .then((data) => {
-    //     res.json(data);
-    //     helpers.log({
-    //         message: "Successfully create post",
-    //         data
-    //     }, false);
-    // })
-    // .catch((error) => res.json(error));
+    helpers.createPost({...req.body, author: req.user.name})
+    .then((data) => {
+        res.json(data);
+        helpers.log({
+            message: "Successfully create post",
+            data
+        }, false);
+    })
+    .catch((error) => res.json(error));
 });
 
 function authenticate(req, res, next) {
